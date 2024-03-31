5f77c19fd378619b28a3fa8a19e2cfa9ebc5acb7
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-03-31 09:54:32 +0530

    Create StudentRegistedCourseController.java

---
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/controller/StudentRegistedCourseController.java
@@ -0,0 +1,9 @@
+package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+public class StudentRegistedCourseController
+{
+
+}
