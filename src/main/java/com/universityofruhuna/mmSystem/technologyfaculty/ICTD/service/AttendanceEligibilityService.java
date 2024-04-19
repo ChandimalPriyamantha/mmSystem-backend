d33c2777ea59e32ff4870c63ec2fc4148c25a620
GANIDU2413 <ganidusahan@gmail.com>
2024-04-20 01:37:29 +0530

    Update AttendanceEligibilityService.java

--- src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/service/AttendanceEligibilityService.java
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/service/AttendanceEligibilityService.java
@@ -1,17 +1,26 @@
 package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.service;
 
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.AttendanceEligibilityDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.MedicalDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.ResponseDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.Util.VarList;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.dao.AttendanceEligibilityRepo;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.AttendanceEligibility;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 public class AttendanceEligibilityService
 {
+    @Autowired
+    ResponseDTO responseDTO;
     @Autowired
     private AttendanceEligibilityRepo attendanceEligibilityRepo;
 
@@ -31,4 +40,30 @@ public class AttendanceEligibilityService
         return modelMapper.map(list, AttendanceEligibilityDTO.class);
 
     }
+
+
+    public ResponseDTO getAllAttendance(){
+        List<AttendanceEligibility> attendanceEligibilities = attendanceEligibilityRepo.findAll();
+        if (attendanceEligibilities.isEmpty()){
+            responseDTO.setCode(VarList.RIP_NO_DATA_FOUND);
+            responseDTO.setContent(null);
+            responseDTO.setMessage("Attendances not found!");
+        }else {
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(modelMapper.map(attendanceEligibilities, new TypeToken<ArrayList<AttendanceEligibilityDTO>>(){}.getType()));
+            responseDTO.setMessage("Attendances found!");
+        }
+        return responseDTO;
+    }
+    public ResponseDTO insertAttendancesAsBulk(List<AttendanceEligibilityDTO> attendanceEligibilityDTOS){
+        
+    }
+    public ResponseDTO insertAAttendance(){}
+    public ResponseDTO getAAttendanceById(){}
+    public ResponseDTO updateAAttendanceById(){}
+    public ResponseDTO deleteAAttendanceById(){}
+
+
+
+
 }
