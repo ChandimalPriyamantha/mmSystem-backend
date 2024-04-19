fffacd27ba1f3da6d49792cdf0078314c84e8988
GANIDU2413 <ganidusahan@gmail.com>
2024-04-20 04:39:31 +0530

    Update StudentDetailsService.java

--- src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/service/StudentDetailsService.java
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/service/StudentDetailsService.java
@@ -1,6 +1,8 @@
 package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.service;
 
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.ResponseDTO;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.StudentDetailsDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.Util.VarList;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.dao.StudentDetailsRepo;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.StudentDetailsEntity;
 import jakarta.transaction.Transactional;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,9 +24,97 @@ public class StudentDetailsService {
     @Autowired
     private ModelMapper mmp;
 
-    public void SaveStudentDetails(List <StudentDetailsDTO> studentDetailsList){
-        studentDetailsRepo.saveAll(mmp.map( studentDetailsList,new TypeToken<ArrayList <StudentDetailsEntity>>(){}.getType()));
+    @Autowired
+    ResponseDTO responseDTO;
 
+    public ResponseDTO getAllStudentsD(){
+        List<StudentDetailsEntity> courseEntities = studentDetailsRepo.findAll();
+        if (courseEntities.isEmpty()){
+            responseDTO.setCode(VarList.RIP_NO_DATA_FOUND);
+            responseDTO.setContent(null);
+            responseDTO.setMessage("Students Details not found!");
+        }else {
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(mmp.map(courseEntities, new TypeToken<ArrayList<StudentDetailsDTO>>(){}.getType()));
+            responseDTO.setMessage("Students Details found!");
+        }
+        return responseDTO;
+    }
+    public ResponseDTO insertStudentDetailsAsBulk(List <StudentDetailsDTO> studentDetailsDTOS){
+        List<StudentDetailsEntity> studentDetailsAsBulk = mmp.map( studentDetailsDTOS,new TypeToken<ArrayList <StudentDetailsEntity>>(){}.getType());
+        try {
+            studentDetailsRepo.saveAll(studentDetailsAsBulk);
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(studentDetailsDTOS);
+            responseDTO.setMessage("Students Details have been inserted");
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RIP_ERROR);
+            responseDTO.setContent(studentDetailsDTOS);
+            responseDTO.setMessage(e.getMessage());
+        }
+        return responseDTO;
+    }
+
+    public ResponseDTO insertAStudentD(StudentDetailsDTO studentDetailsDTO){
+        StudentDetailsEntity insertOneStudentD = mmp.map(studentDetailsDTO,StudentDetailsEntity.class);
+        try {
+            studentDetailsRepo.save(insertOneStudentD);
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(studentDetailsDTO);
+            responseDTO.setMessage("A Student Details has been inserted");
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RIP_ERROR);
+            responseDTO.setContent(studentDetailsDTO);
+            responseDTO.setMessage(e.getMessage());
+        }
+        return responseDTO;
+    }
+
+    public ResponseDTO getAStudentDById(int id){
+        if (studentDetailsRepo.existsById(id)){
+
+            Optional<StudentDetailsEntity> StudentDById = studentDetailsRepo.findById(id);
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(StudentDById);
+            responseDTO.setMessage("Data found");
+
+        }else {
+            responseDTO.setCode(VarList.RIP_NO_DATA_FOUND);
+            responseDTO.setContent(null);
+            responseDTO.setMessage("Data not found");
+        }
+
+        return responseDTO;
+    }
+
+    public ResponseDTO updateAStudentDById(StudentDetailsDTO studentDetailsDTO){
+        StudentDetailsEntity updateOneStudentDById = mmp.map(studentDetailsDTO,StudentDetailsEntity.class);
+        try {
+            studentDetailsRepo.save(updateOneStudentDById);
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(studentDetailsDTO);
+            responseDTO.setMessage("The Student Details has been updated");
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RIP_ERROR);
+            responseDTO.setContent(studentDetailsDTO);
+            responseDTO.setMessage(e.getMessage());
+        }
+        return responseDTO;
+
+    }
+
+    public ResponseDTO deleteAStudentDById(int id){
+        if (studentDetailsRepo.existsById(id)){
+            studentDetailsRepo.deleteById(id);
+            responseDTO.setCode(VarList.RIP_SUCCESS);
+            responseDTO.setContent(id);
+            responseDTO.setMessage("The Student Details has been deleted");
+        }else {
+            responseDTO.setCode(VarList.RIP_ERROR);
+            responseDTO.setContent(id);
+            responseDTO.setMessage("The Student Details id not found");
+        }
+        return responseDTO;
     }
 
 }
