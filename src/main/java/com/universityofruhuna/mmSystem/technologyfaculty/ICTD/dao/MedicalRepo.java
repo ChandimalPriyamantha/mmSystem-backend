f1f48061e6c1dd96b71b1ea98e243eb6d9c9a753
GANIDU2413 <ganidusahan@gmail.com>
2024-04-19 22:56:34 +0530

    Update MedicalRepo.java

--- src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/dao/MedicalRepo.java
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/dao/MedicalRepo.java
@@ -3,5 +3,6 @@ package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.dao;
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.MedicalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MedicalRepo extends JpaRepository<MedicalEntity,Integer> {
 }
