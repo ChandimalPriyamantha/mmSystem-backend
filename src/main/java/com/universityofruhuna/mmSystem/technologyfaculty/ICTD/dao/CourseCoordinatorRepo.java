90fc4840f96da4247787f4e455b8dce5aa890037
GANIDU2413 <ganidusahan@gmail.com>
2024-04-20 12:08:53 +0530

    Create CourseCoordinatorRepo.java

---
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/dao/CourseCoordinatorRepo.java
@@ -0,0 +1,7 @@
+package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.dao;
+
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.CourseCoordinatorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseCoordinatorRepo extends JpaRepository<CourseCoordinatorEntity,Integer> {
+}
