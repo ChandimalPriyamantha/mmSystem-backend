212bf5e268b111901416147a48a842df414c2738
GANIDU2413 <ganidusahan@gmail.com>
2024-04-24 17:01:36 +0530

    Update CourseCoordinatorRepo.java

--- src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/dao/CourseCoordinatorRepo.java
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/dao/CourseCoordinatorRepo.java
@@ -2,6 +2,11 @@ package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.dao;
 
 import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.CourseCoordinatorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseCoordinatorRepo extends JpaRepository<CourseCoordinatorEntity,Integer> {
+
+    @Query(nativeQuery = true, value = "select course_id from coursecoordinator where  user_id=: user_id")
+    CourseCoordinatorEntity findCourseIDByUserID(@Param("user_id") String user_id);
 }
