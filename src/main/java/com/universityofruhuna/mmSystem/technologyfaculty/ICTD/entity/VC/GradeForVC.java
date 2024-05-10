bfbf8c0e8af459ca0a050d35bb221fa6c7111bc7
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-05-11 02:27:39 +0530

    develop lecture

---
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/entity/VC/GradeForVC.java
@@ -0,0 +1,33 @@
+package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.entity.VC;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "grade")
+public class GradeForVC {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "student_id")
+    private String studentId;
+
+    @Column(name = "course_id")
+    private String courseId;
+
+    @Column(name = "level")
+    private String level;
+
+    @Column(name = "semester")
+    private String semester;
+
+    @Column(name = "overal_score")
+    private String overallScore;
+
+    @Column(name = "grade")
+    private String grade;
+}
