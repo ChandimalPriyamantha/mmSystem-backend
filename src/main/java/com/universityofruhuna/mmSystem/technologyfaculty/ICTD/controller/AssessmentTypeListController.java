3651e691505f57040a7138132ad265104248c698
GANIDU2413 <ganidusahan@gmail.com>
2024-05-18 02:34:05 +0530

    Create AssessmentTypeListController.java

---
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/controller/AssessmentTypeListController.java
@@ -0,0 +1,27 @@
+package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.controller;
+
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.AssessmentTypeListDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO.ResponseDTO;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.Util.VarList;
+import com.universityofruhuna.mmSystem.technologyfaculty.ICTD.service.AssessmentTypeListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("api/astylist")
+public class AssessmentTypeListController {
+
+    @Autowired
+    AssessmentTypeListService assessmentTypeListService;
+
+    @PostMapping("savenewasty")
+    public ResponseEntity saveNewAssessmentType(@RequestBody AssessmentTypeListDTO assessmentTypeListDTO){
+        ResponseDTO responseDTO = assessmentTypeListService.NewAssessmentType(assessmentTypeListDTO);
+        if (responseDTO.getCode().equals(VarList.RIP_SUCCESS)) return new ResponseEntity(responseDTO, HttpStatus.CREATED);
+        else return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+    }
+}
