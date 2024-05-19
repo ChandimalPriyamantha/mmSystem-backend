4a38252b01744406577b9c57ff8f49cca27ae7b9
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-05-19 23:26:02 +0530

    Create StudentData.java

---
+++ src/main/java/com/universityofruhuna/mmSystem/technologyfaculty/ICTD/DTO/StudentData.java
@@ -0,0 +1,35 @@
+package com.universityofruhuna.mmSystem.technologyfaculty.ICTD.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Data
+public class StudentData
+{
+    private String student_id;
+    private String student_name;
+
+    private List<ObjectDTO> ca;
+
+    private String total_ca_mark;
+
+    private List<ObjectDTO> end;
+
+    private String total_final_marks;
+
+    private String total_rounded_marks;
+
+    private String grade;
+
+    private String gpv;
+
+    private String ca_eligibility;
+}
